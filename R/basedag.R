aa42eb8483f315f10a3983ba76225513310adc77
Maximilian Scholz <m0hawk@gmx.de>
2022-01-31 16:20:02 +0100

    Add basedag data simulation and model calibration

---
+++ R/basedag.R
@@ -0,0 +1,88 @@
+#' Title
+#'
+#' @param data_N
+#' @param z1_x_coef
+#' @param z3_x_coef
+#' @param x_y_coef
+#' @param z1_y_coef
+#' @param z2_y_coef
+#' @param y_z4_coef
+#' @param x_z4_coef
+#' @param y_intercept
+#' @param sigma_z1
+#' @param sigma_z2
+#' @param sigma_z3
+#' @param sigma_z4
+#' @param sigma_x
+#' @param sigma_y
+#' @param data_link
+#' @param RNG
+#' @param lb
+#' @param ub
+#' @param oversampling
+#'
+#' @return
+#' @export
+#'
+#' @examples
+basedag_data <- function(data_N,
+                         z1_x_coef = 1,
+                         z3_x_coef = 1,
+                         x_y_coef = 1,
+                         z1_y_coef = 1,
+                         z2_y_coef = 1,
+                         y_z4_coef = 1,
+                         x_z4_coef = 1,
+                         y_intercept = 0,
+                         sigma_z1 = 1,
+                         sigma_z2 = 1,
+                         sigma_z3 = 1,
+                         sigma_z4 = 1,
+                         sigma_x = 1,
+                         sigma_y = 1,
+                         data_link,
+                         RNG,
+                         lb,
+                         ub,
+                         oversampling = 10) {
+  n <- data_N * oversampling
+  z1 <- rnorm(n, sigma_z1)
+  z2 <- rnorm(n, sigma_z2)
+  z3 <- rnorm(n, sigma_z3)
+
+  x <- rnorm(n, mean = (z1_x_coef * z1 + z3_x_coef * z3), sd = sigma_x)
+  y <- RNG(
+    n,
+    data_link(
+      y_intercept +
+        x_y_coef * x +
+        z1_y_coef * z1 +
+        z2_y_coef * z2
+    ),
+    sigma_y
+  )
+  z4 <- rnorm(n, mean = (y_z4_coef * y + x_z4_coef * x), sd = sigma_z4)
+
+  good_indices <- seq_along(y)
+  if (lb) {
+    good_indices <- intersect(good_indices, which(y > lb))
+  }
+  if (ub) {
+    good_indices <- intersect(good_indices, which(y < ub))
+  }
+  if (length(good_indices) < data_N) {
+    stop("Even with resampling, there were not enough usable samples during
+         data generation. Try increasing the oversampling factor.")
+  }
+  good_indices <- good_indices[1:data_N]
+  return(
+    data.frame(
+      x = x[good_indices],
+      z1 = z1[good_indices],
+      z2 = z2[good_indices],
+      z3 = z3[good_indices],
+      z4 = z4[good_indices],
+      y = y[good_indices]
+    )
+  )
+}
---
+++ R/calibration.R
@@ -0,0 +1,205 @@
+#' Title
+#'
+#' @param model_name
+#' @param results
+#' @param samples
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calibration_result_text <- function(model_name, results, samples) {
+  print(model_name)
+  greater_lower_ci <- unlist(
+    lapply(
+      results$greater,
+      function(x) {
+        x$hypothesis$CI.Lower
+      }
+    )
+  )
+  print(
+    paste(
+      length(greater_lower_ci[greater_lower_ci < 0]),
+      "/",
+      samples,
+      " of 'x>0' lower CI bounds are < 0",
+      sep = ""
+    )
+  )
+
+  equal_lower_ci <- unlist(
+    lapply(
+      results$equal,
+      function(x) {
+        x$hypothesis$CI.Lower
+      }
+    )
+  )
+  print(
+    paste(
+      length(equal_lower_ci[equal_lower_ci < 0]),
+      "/",
+      samples,
+      " of 'x=0' lower CI bounds are < 0",
+      sep = ""
+    )
+  )
+  print("=================================================")
+}
+
+#' Title
+#'
+#' @param dataset
+#' @param prefit
+#'
+#' @return
+#' @export
+#'
+#' @examples
+parallel_run <- function(dataset, prefit) {
+  fit <- stats::update(prefit,
+    newdata = dataset,
+    refresh = 0,
+    silent = 2,
+    chains = 4,
+    control = list(adapt_delta = 0.9),
+    prior = c(
+      brms::prior("", class = "Intercept")
+    )
+  )
+  return(
+    list(
+      greater = brms::hypothesis(fit, hypothesis = "x>0"),
+      equal = brms::hypothesis(fit, hypothesis = "x=0")
+    )
+  )
+}
+
+#' Title
+#'
+#' @param data_list
+#' @param brms_formular
+#' @param ncores
+#' @param brms_family
+#' @param stanvars
+#' @param exports
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calibrate_formula <- function(data_list,
+                              brms_formular,
+                              ncores,
+                              brms_family,
+                              stanvars,
+                              exports) {
+  prefit <- brms::brm(
+    brms_formular,
+    data = data_list[[1]],
+    family = brms_family,
+    stanvars = stanvars,
+    chains = 0,
+    refresh = 0,
+    silent = 2,
+    control = list(adapt_delta = 0.9),
+    prior = c(
+      brms::prior("", class = "Intercept")
+    )
+  )
+
+  cluster <- parallel::makeCluster(ncores)
+  doParallel::registerDoParallel(cluster)
+  `%dopar%` <- foreach::`%dopar%`
+
+  results <- foreach::foreach(
+    dataset = data_list,
+    .packages = c("brms"),
+    .export = exports
+  ) %dopar% {
+    parallel_run(dataset, prefit)
+  }
+  parallel::stopCluster(cluster)
+
+  return(
+    list(
+      greater = lapply(results, function(x) {
+        x$greater
+      }),
+      equal = lapply(results, function(x) {
+        x$equal
+      })
+    )
+  )
+}
+
+#' Title
+#'
+#' @param dataset_N
+#' @param ncores
+#' @param brms_family
+#' @param stanvars
+#' @param exports
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calibrate_family <- function(dataset_N,
+                             ncores,
+                             brms_family,
+                             stanvars,
+                             exports,
+                             ...) {
+  data_list <- vector("list", length = dataset_N)
+  for (i in seq_len(dataset_N)) {
+    data_list[i] <- list(basedag_data(...))
+  }
+  hist(data_list[[1]]$y, main = "data", xlab = "data")
+  true_model_logit_sim <- calibrate_formula(data_list,
+    brms_formular = brms::brmsformula(y ~ x + z1 + z2),
+    ncores = ncores,
+    brms_family = brms_family,
+    stanvars = stanvars,
+    exports = exports
+  )
+  calibration_result_text(model_name = "True Model", results = true_model_logit_sim, samples = dataset_N)
+
+  z1_model_logit_sim <- calibrate_formula(data_list,
+    brms_formular = brms::brmsformula(y ~ x + z2),
+    ncores = ncores,
+    brms_family = brms_family,
+    stanvars = stanvars,
+    exports = exports
+  )
+  calibration_result_text(model_name = "z1 Model", results = z1_model_logit_sim, samples = dataset_N)
+
+  z2_model_logit_sim <- calibrate_formula(data_list,
+    brms_formular = brms::brmsformula(y ~ x + z1),
+    ncores = ncores,
+    brms_family = brms_family,
+    stanvars = stanvars,
+    exports = exports
+  )
+  calibration_result_text(model_name = "z2 Model", results = z2_model_logit_sim, samples = dataset_N)
+
+  z3_model_logit_sim <- calibrate_formula(data_list,
+    brms_formular = brms::brmsformula(y ~ x + z1 + z2 + z3),
+    ncores = ncores,
+    brms_family = brms_family,
+    stanvars = stanvars,
+    exports = exports
+  )
+  calibration_result_text(model_name = "z3 Model", results = z3_model_logit_sim, samples = dataset_N)
+
+  z4_model_logit_sim <- calibrate_formula(data_list,
+    brms_formular = brms::brmsformula(y ~ x + z1 + z2 + z4),
+    ncores = ncores,
+    brms_family = brms_family,
+    stanvars = stanvars,
+    exports = exports
+  )
+  calibration_result_text(model_name = "z4 Model", results = z4_model_logit_sim, samples = dataset_N)
+}
