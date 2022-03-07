947381a8adb8a13312c6f14d86cf76a74ef1f3e7
Maximilian Scholz <m0hawk@gmx.de>
2022-03-07 11:54:35 +0100

    Add Post.Prop output to hypotheses

--- R/hypothesis_helper.R
+++ R/hypothesis_helper.R
@@ -12,6 +12,7 @@ hypothesis_cis <- function(hyp, fit, ...) {
   result <- brms::hypothesis(fit, hyp)
   return(list(
     "lower_ci" = result$hypothesis$CI.Lower,
-    "upper_ci" = result$hypothesis$CI.Upper
+    "upper_ci" = result$hypothesis$CI.Upper,
+    "post_prop" = result$hypothesis$Post.Prob
   ))
 }
