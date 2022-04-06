38629a5d9f2e17430ef10d099ee0a73f73223f77
Yannik.Dzubba <yannick@dzubba.net>
2022-04-06 13:00:03 +0200

    Added gompertz custom family

---
+++ R/gompertz.R
@@ -0,0 +1,200 @@
+#' Title
+#'
+#' @param x Model space, defined for x >= 0
+#' @param mu Median parameter of pdf, mu > 0
+#' @param eta Second shape parameter of Gompertz, defined for eta > 1
+#' @param log Optional argument. If true, returns log(pdf). Normally False.
+#'
+#' @return pdf of gompertz distribution, with median parameterasation
+#' @export
+#'
+#' @examples x <- seq(from = 0, to = 5, length.out = 100)
+#' eta <- 0.1
+#' b <- 1
+#' median <- (1 / b) * log((-1 / eta) * log(1 / 2) + 1)
+#' y <- bayesim::dgompertz(x, mu = median, eta = eta)
+#' plot(x, y, type = "l", ylab = "Density", main = "dgompertz(mu=2.0708, eta=0.1) or dgompertz(b=1, eta=0.1)")
+#' # Compare to online ressources
+dgompertz <- function(x, mu, eta, log = FALSE) {
+  # check arguments
+  if (isTRUE(mu <= 0)) {
+    stop("The Gompertz PDF is only defined for mu > 0")
+  }
+  if (isTRUE(eta <= 0)) {
+    stop("The Gompertz PDF is only defined for eta > 0")
+  }
+  if (isTRUE(any(x < 0))) {
+    stop("The Gompertz PDF is only defined on the positive scale")
+  }
+
+  # calculate missing argument b
+  # median = (1/b) * ln[(-1/eta)*ln(1/2)+1]
+  b <- (1 / mu) * log1p((-1 / eta) * log(0.5))
+  log_b <- -log(mu) + log(log1p((-1 / eta) * log(0.5)))
+  # calculate log-pdf with pdf = b * eta * exp(eta + bx - eta * exp(bx))
+  lpdf <- log_b + log(eta) + (eta + b * x - eta * exp(b * x))
+
+  # now return either log or normal value
+  if (log) {
+    return(lpdf)
+  } else {
+    return(exp(lpdf))
+  }
+}
+
+#' Title
+#'
+#' @param p Quantile to be calculated
+#' @param mu Median argument of Gompertz
+#' @param eta Eta argument of Gompertz
+#'
+#' @return Inverse of CDF, calculates a value, given a probability p
+#' @export
+#'
+#' @examples x <- seq(from = 0, to = 1, length.out = 100)
+#' plot(x, bayesim::qgompertz(x, mu = 2, eta = 0.1), type = "l", ylab = "Quantile", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
+qgompertz <- function(p, mu, eta) {
+  # check arguments
+  if (isTRUE(mu <= 0)) {
+    stop("The Gompertz Qunatile-function is only defined for mu > 0")
+  }
+  if (isTRUE(eta <= 0)) {
+    stop("The Gompertz Qunatile-function is only defined for eta > 0")
+  }
+  if (isTRUE(any(p < 0))) {
+    stop("The Gompertz Qunatile-function is only defined on the positive scale")
+  }
+
+  # calculate missing argument b
+  # median = (1/b) * ln[(-1/eta)*ln(1/2)+1]
+  b <- (1 / mu) * log1p((-1 / eta) * log(0.5))
+
+  # calculate the QDF as inverse of CDF
+  x <- (1 / b) * log1p(-(1 / eta) * log1p(-p))
+
+  return(x)
+}
+
+#' Title
+#'
+#' @param n Number of draws
+#' @param mu Median argument of Gompertz
+#' @param eta Eta argument of Gompertz
+#'
+#' @return A Gompertz distributed RNG vector of size n
+#' @export
+#'
+#' @examples y <- bayesim::rgompertz(n, mu = 2, eta = 0.1)
+#' hist(y, main = c(paste("Median:", median(y)), " for RNG of apex-after-origin Gompertz(mu=2, eta=0.1)"))
+rgompertz <- function(n, mu, eta) {
+  # check arguments
+  if (isTRUE(mu <= 0)) {
+    stop("The Gompertz RNG is only defined for mu > 0")
+  }
+  if (isTRUE(eta <= 0)) {
+    stop("The Gompertz RNG is only defined for eta > 0")
+  }
+  return(qgompertz(runif(n, min = 0, max = 1), mu = mu, eta = eta))
+}
+
+#' Title
+#'
+#' @param i BRMS indices
+#' @param prep BRMS data
+#'
+#' @return Log-Likelihood of gompertz given data in prep
+#'
+#' @examples
+log_lik_gompertz <- function(i, prep) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  eta <- brms::get_dpar(prep, "eta", i = i)
+  y <- prep$data$Y[i]
+  return(dgompertz(y, mu, eta, log = TRUE))
+}
+
+#' Title
+#'
+#' @param i BRMS indices
+#' @param prep BRMS data
+#' @param ...
+#'
+#' @return Posterior prediction of gompertz, given data in prep
+#'
+#' @examples
+posterior_predict_gompertz <- function(i, prep, ...) {
+  mu <- brms::get_dpar(prep, "mu", i = i)
+  eta <- brms::get_dpar(prep, "eta", i = i)
+  return(rgompertz(prep$ndraws, mu, eta))
+}
+
+# ei_expint <- function(x, scale = FALSE)
+#  -.External(C_expint_do_expint, "E1", -x, scale)
+# found as part of: https://cran.r-project.org/web/packages/expint/index.html
+
+#' Title
+#'
+#' @param prep BRMS data
+#'
+#' @return Recover the given mean of data prep
+#'
+#' @examples
+# posterior_epred_gompertz <- function(prep) {
+#   # check for necessary package
+#   if(!installed.packages())
+#   # get the arguments out of the prep data
+#   mu <- get_dpar(prep, "mu")
+#   eta <- get_dpar(prep, "eta")
+#   # recover the missing b argument
+#   b <- (1 / mu) * log1p((-1 / eta) * log(0.5))
+#   # calculate the mean
+#   mean <- (1 / b) * exp(eta) * ei_expint(-eta)
+#   return(mean)
+# }
+posterior_epred_gompertz <- function(prep) {
+  stop("posterior_epred is not defined for the gomperz family")
+}
+
+
+#'
+#' Title
+#'
+#' @param link Link function for function
+#' @param link_eta Link function for eta argument
+#'
+#' @return BRMS gompertz distribution family
+#' @export
+#'
+#' @examples data <- list(a = a, y = bayesim::rgompertz(n, exp(0.5 * a + 1), 0.2))
+#' fit1 <- brm(y ~ 1 + a,
+#'   data = data, family = bayesim::gompertz(),
+#'   stanvars = bayesim::gompertz()$stanvars, backend = "cmdstan"
+#' )
+#' plot(fit1)
+gompertz <- function(link = "log", link_eta = "log") {
+  family <- brms::custom_family(
+    "gompertz",
+    dpars = c("mu", "eta"),
+    links = c(link, link_eta),
+    lb = c(0, 0),
+    ub = c(NA, NA),
+    type = "real",
+    log_lik = log_lik_gompertz,
+    posterior_predict = posterior_predict_gompertz,
+    posterior_epred = posterior_epred_gompertz
+  )
+  family$stanvars <- brms::stanvar(
+    scode = "
+      real gompertz_lpdf(real y, real mu, real eta) {
+        real b = (1 / mu) * log1p((-1 / eta) * log(0.5));
+        return((-log(mu) + log(log1p((-1 / eta) * log(0.5)))) +
+               log(eta) + (eta + b * y - eta * exp(b * y)));
+      }
+      real gompertz_rng(real mu, real eta) {
+        return((1 / ((1 / mu) * log1p(-(1 / eta) * log(0.5)))) *
+                log1p((-1 / eta) *
+                log(uniform_rng(0, 1))));
+      }",
+    block = "functions"
+  )
+  return(family)
+}
