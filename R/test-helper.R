0c9221d7366a17a5a796c0b7ca2d3c417529fd06
Yannick73 <yannick@dzubba.net>
2022-04-27 11:11:54 +0200

    Enhanced documentation of the testthat custom expect functions.

--- R/test-helper.R
+++ R/test-helper.R
@@ -5,6 +5,17 @@
 #' @param b scalar or vector b to be compared
 #' @param eps scalar or vector, setting the max differences, eps > 0
 #'
+#' For vector/scalar combinations, allowed are:
+#' all scalar
+#' a vector, b scalar, eps scalar -> compare a values to be close enough to b
+#' a scalar, b vector, eps scalar -> compare b values to be close enough to a
+#' a scalar, b scalar, eps vector -> compare the difference a, b too all eps
+#' -> case not intended, but would work
+#' a vector, b scalar, eps vector -> compare each vector-difference entry against each eps
+#' a vector, b scalar, eps vector -> compare each vector-difference entry against each eps
+#' all vectors -> each entry of |a - b| is compared to the same entry in eps
+#' -> different vector lengths != 1 dissallowed!
+#'
 #' @return expect_true(a close enought to b)
 #' @export
 #'
@@ -23,7 +34,7 @@ expect_eps <- function(a, b, eps) {
   # then check, that
   # first check, that all eps are > 0
   if(isTRUE(any(eps <= 0)))
-    stop("Tried checking against negative/zero differences")
+    stop("Tried checking against negative or zero differences")
 
   # then check, if vectors are of same length
   vector_length <- max(length(a), length(b), length(eps))
@@ -35,8 +46,8 @@ expect_eps <- function(a, b, eps) {
     stop("Used different length of vectors in test")
 
   # last check, the actual value difference
-  bool_comparison <- abs(a - b) < eps
-  if(isTRUE(all(bool_comparison)))
+  eps_comparison <- abs(a - b) < eps
+  if(isTRUE(all(eps_comparison)))
     succeed()
   else
     fail("At least one comparison between a and b had a bigger difference, then eps")
@@ -44,6 +55,13 @@ expect_eps <- function(a, b, eps) {
 
 #' Check, if a is bigger than b. expect_smaller(a, b) can be done,
 #' by expect_bigger(b, a) with swapped arguments.
+#'
+#' For vector/scalar combinations, allowed are:
+#' both scalar
+#' a scalar, b vector -> compare a to all b values
+#' a vector, b scalar -> compare all a values to the threshold b
+#' both vectors -> compare each entry to the other entry
+#' -> different lengths != 1 dissallowed!
 #'
 #' @param a number to be tested, should be real scalar or vector
 #' @param b tested against, should be real scalar or vector
@@ -58,19 +76,23 @@ expect_eps <- function(a, b, eps) {
 #' expect_bigger(c(1, 2), c(0, 3)) should fail
 #' expect_bigger(c(1, 2, 3), c(0, 1)) should produce an error
 expect_bigger <- function(a, b) {
+
+  # check, that both are numerical data
   if(!is.numeric(a) || !is.numeric(b))
     stop("All inputs have to be numeric data.")
 
+  # check, if vectors both are vectors, both are of equal length
   vector_length <- max(length(a), length(b))
   a_wrong <- length(a) > 1 && length(a) != vector_length
   b_wrong <- length(b) > 1 && length(b) != vector_length
   if(a_wrong || b_wrong)
-    stop("Used different length of vectors in test")
+    stop("Used different lengths of vectors in test")
 
-  bool_comparison <- all(a > b)
-  if(isTRUE(bool_comparison))
+  # now compare all a and b values
+  all_bigger <- all(a > b)
+  if(isTRUE(all_bigger))
     succeed()
   else
-    fail("At least one number was smaller, than b")
+    fail("At least one number a was smaller than b")
 
 }
