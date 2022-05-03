9d7d861700f9b687bd3d8e4ccc357c9b6226698a
Yannick73 <yannick@dzubba.net>
2022-05-03 19:02:32 +0200

    Fixed bug in gompertz_lpdf-Stan-code (did not fit well sometimes). Also enhanced gompertz dev-test.

--- Tests/gompertz_test.R
+++ Tests/gompertz_test.R
@@ -60,10 +60,12 @@ hist(data$y)
 
 fit1 <- brm(
   y ~ 1 + a,
+  init = 0.1,
   data = data,
   family = bayesim::gompertz(),
   stanvars = bayesim::gompertz()$stanvars,
-  backend = "cmdstan"
+  backend = "cmdstan",
+  cores = 4
 )
 
 summary(fit1)
