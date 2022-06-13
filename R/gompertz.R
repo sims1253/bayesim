d2724c710046a5bd9ad55889be253f9ed39d5ddd
Maximilian Scholz <m0hawk@gmx.de>
2022-06-13 19:21:30 +0200

    Improve gompertz distribution

--- R/gompertz.R
+++ R/gompertz.R
@@ -1,22 +1,31 @@
 #' Probability density function for the Gompertz distribution, with Median parametrization.
 #'
-#' @param x Model space, defined for x >= 0
-#' @param mu Median parameter of pdf, mu > 0
-#' @param eta Second shape parameter of Gompertz, defined for eta > 1
-#' @param log Optional argument. If TRUE, returns log(pdf). Normally False.
+#' @param x Value
+#' @param mu Median parameter
+#' @param beta Scale parameter
+#' @param log Optional argument. If TRUE, returns log(pdf).
 #'
 #' @return PDF of gompertz distribution, with median parametrization.
 #' @export
 #'
 #' @examples x <- seq(from = 0, to = 5, length.out = 100)
-#' eta <- 0.1
-#' b <- 1
-#' median <- (1 / b) * log((-1 / eta) * log(1 / 2) + 1)
-#' y <- bayesim::dgompertz(x, mu = median, eta = eta)
-#' plot(x, y, type = "l", ylab = "Density", main = "dgompertz(mu=2.0708, eta=0.1) or dgompertz(b=1, eta=0.1)")
+#' y <- bayesim::dgompertz(x, mu = 10, beta = 10)
+#' plot(x, y, type = "l", ylab = "Density", main = "dgompertz(mu=10, beta=10)")
 #' # Compare to online ressources
-dgompertz <- function(x, mu, b, log = FALSE) {
-  lpdf <- log(-b * log(0.5)) - log(exp(mu * b) - 1) + b * x - (-log(0.5)/(exp(mu * b) - 1)) * (exp(b * x) - 1)
+dgompertz <- function(x, mu, beta, log = FALSE) {
+  # check the arguments
+  if (isTRUE(any(x <= 0))) {
+    stop("dgompertz is only defined for x > 0")
+  }
+  if (isTRUE(mu <= 0)) {
+    stop("dgompertz is only defined for mu > 0")
+  }
+  if (isTRUE(beta <= 0)) {
+    stop("dgompertz is only defined for beta > 0")
+  }
+  lpdf <- log(-beta * log(0.5)) -
+    log(exp(mu * beta) - 1) +
+    (beta * x + (log(0.5) / (exp(mu * beta) - 1)) * (exp(beta * x) - 1))
   if (log) {
     return(lpdf)
   } else {
@@ -24,38 +33,56 @@ dgompertz <- function(x, mu, b, log = FALSE) {
   }
 }
 
-
 #' Quantile function for the Gompertz distribution, with Median parametrization.
 #'
 #' @param p Quantile to be calculated
-#' @param mu Median argument of Gompertz
-#' @param eta Eta argument of Gompertz
+#' @param mu Median parameter
+#' @param beta Scale parameter
 #'
 #' @return Inverse of CDF, calculates a value, given a probability p
 #' @export
 #'
 #' @examples x <- seq(from = 0, to = 1, length.out = 100)
-#' plot(x, bayesim::qgompertz(x, mu = 2, eta = 0.1), type = "l", ylab = "Quantile", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
-qgompertz <- function(p, mu, b) {
-  a <- -(b * log(0.5)) / (exp(mu * b) - 1)
-  x <- (1 / b) * log1p(-(b/a) * log1p(-p))
-  return(x)
+#' plot(x, bayesim::qgompertz(x, mu = 10, beta = 1), type = "l", ylab = "Quantile", main = "apex-after-origin Gompertz(mu=10, beta=1)")
+qgompertz <- function(p, mu, beta) {
+  # check the arguments
+  if (isTRUE(any(p <= 0 | p >= 1))) {
+    stop("qgompertz is only defined for 0 < p < 1")
+  }
+  if (isTRUE(mu <= 0)) {
+    stop("qgompertz is only defined for mu > 0")
+  }
+  if (isTRUE(beta <= 0)) {
+    stop("qgompertz is only defined for beta > 0")
+  }
+  return(
+    log1p(((exp(mu * beta) - 1) / (log(0.5))) * log1p(-p)) / beta
+  )
 }
 
 #' RNG function for the Gompertz distribution, with Median parametrization.
 #'
 #' @param n Number of draws
-#' @param mu Median argument of Gompertz
-#' @param eta Eta argument of Gompertz
+#' @param mu Median parameter
+#' @param beta Scale parameter
 #'
 #' @return A Gompertz distributed RNG vector of size n
 #' @export
 #'
 #' @examples y <- bayesim::rgompertz(n, mu = 2, eta = 0.1)
 #' hist(y, main = c(paste("Median:", median(y)), " for RNG of apex-after-origin Gompertz(mu=2, eta=0.1)"))
-rgompertz <- function(n, mu, b) {
-  return(qgompertz(runif(n), mu, b))
+rgompertz <- function(n, mu, beta) {
+  if (isTRUE(mu <= 0)) {
+    stop("rgompertz is only defined for mu > 0")
+  }
+  if (isTRUE(beta <= 0)) {
+    stop("rgompertz is only defined for beta > 0")
+  }
+  return(
+    log1p(((exp(mu * beta) - 1) / (log(0.5))) * log(runif(n))) / beta
+  )
 }
+
 
 #' Log-Likelihood vignette for the Gompertz distribution, with Median parametrization.
 #'
@@ -67,9 +94,9 @@ rgompertz <- function(n, mu, b) {
 #' @examples
 log_lik_gompertz <- function(i, prep) {
   mu <- brms::get_dpar(prep, "mu", i = i)
-  b <- brms::get_dpar(prep, "beta", i = i)
+  beta <- brms::get_dpar(prep, "beta", i = i)
   y <- prep$data$Y[i]
-  return(dgompertz(y, mu, b, log = TRUE))
+  return(dgompertz(y, mu, beta, log = TRUE))
 }
 
 #' Posterior-Prediction vignette for the Gompertz distribution, with Median parametrization.
@@ -83,8 +110,8 @@ log_lik_gompertz <- function(i, prep) {
 #' @examples
 posterior_predict_gompertz <- function(i, prep, ...) {
   mu <- brms::get_dpar(prep, "mu", i = i)
-  b <- brms::get_dpar(prep, "beta", i = i)
-  return(rgompertz(prep$ndraws, mu, b))
+  beta <- brms::get_dpar(prep, "beta", i = i)
+  return(rgompertz(prep$ndraws, mu, beta))
 }
 
 #' Expectation-Predict vignette for the Gompertz distribution, with Median parametrization.
@@ -130,15 +157,16 @@ gompertz <- function(link = "log", link_b = "log") {
   family$stanvars <- brms::stanvar(
     scode = "
       real gompertz_lpdf(real y, real mu, real beta) {
-        real log_a = log(-beta * log(0.5)) - log(exp(mu * beta) - 1);
-        real a_div_b = -log(0.5)/(exp(mu * beta) - 1);
-        real lpdf = log_a + beta * y - a_div_b * (exp(beta * y) - 1);
-        return(lpdf);
+        return(
+          log(-beta * log(0.5)) -
+          log(exp(mu * beta) - 1) +
+          (beta * y + (log(0.5) / (exp(mu * beta) - 1)) * (exp(beta * y) - 1))
+        );
       }
       real gompertz_rng(real mu, real beta) {
-        real a = -(beta * log(0.5)) / (exp(mu * beta) - 1);
-        real x = (1 / beta) * log1p(-(beta/a) * log1p(uniform_rng(-1, 0)));
-        return(x);
+      return(
+        log1p(((exp(mu * beta) - 1) / (log(0.5))) * log(uniform_rng(0,1))) / beta
+      );
       }",
     block = "functions"
   )
