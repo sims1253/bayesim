51b188de18ddfe0424cc7963acefbfc097221ec6
Yannik.Dzubba <yannick@dzubba.net>
2022-10-05 17:01:39 +0200

    Fixed Gompertz-Example, works again with new parametrization.

--- examples/gompertz_test.R
+++ examples/gompertz_test.R
@@ -2,54 +2,60 @@ library(bayesim)
 library(extraDistr)
 library(brms)
 
-get_b <- function(mu, eta) {
-  b <- (1 / mu) * log((-1 / eta) * log(1 / 2) + 1)
-  return(b)
-}
-get_a <- function(mu, eta) {
+# get_b <- function(mu, b) {
+#   b <- (1 / mu) * log((-1 / b) * log(1 / 2) + 1)
+#   return(b)
+# }
+# get_a <- function(mu, b) {
+#   # a of extraDistr
+#   a <- get_b(mu, b) * b
+#   return(a)
+# }
+
+get_a <- function(mu, beta) {
   # a of extraDistr
-  a <- get_b(mu, eta) * eta
+  a <- -(beta * log(0.5)) / (exp(mu * beta) - 1)
   return(a)
 }
 
 n <- 10000   # number of testvalues
 
-x = seq(from = 0 , to = 10 , length.out=n)
+x = seq(from = 0.01, to = 10 , length.out=n)
 
 # PDF test
 layout(matrix(1:3, ncol = 3))
-plot(x, bayesim::dgompertz(x, mu = 2, eta = 0.1), type="l", ylab = "Density", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
-plot(x, bayesim::dgompertz(x, mu = 1, eta = 1), type="l", ylab = "Density", main = "apex-at-origin Gompertz(mu=1, eta=1)")
-plot(x, bayesim::dgompertz(x, mu = 2, eta = 3), type="l", ylab = "Density", main = "no-apex Gompertz(mu=2, eta=3)")
+plot(x, bayesim::dgompertz(x, mu = 2, b = 0.1), type="l", ylab = "Density", main = "Gompertz(mu=2, b=0.1)")
+plot(x, bayesim::dgompertz(x, mu = 1, b = 1), type="l", ylab = "Density", main = "Gompertz(mu=1, b=1)")
+plot(x, bayesim::dgompertz(x, mu = 2, b = 3), type="l", ylab = "Density", main = "Gompertz(mu=2, b=3)")
 
 # PDF comparison to a reference
 layout(matrix(1:3, ncol = 3))
-plot(x, bayesim::dgompertz(x, mu = 2, eta = 0.1) - extraDistr::dgompertz(x, get_a(2, 0.1), get_b(2, 0.1)), ylab = "Density difference", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
-plot(x, bayesim::dgompertz(x, mu = 1, eta = 1) - extraDistr::dgompertz(x, get_a(1, 1), get_b(1, 1)), ylab = "Density difference", main = "apex-at-origin Gompertz(mu=1, eta=1)")
-plot(x, bayesim::dgompertz(x, mu = 2, eta = 3) - extraDistr::dgompertz(x, get_a(2, 3), get_b(2, 3)), ylab = "Density difference", main = "no-apex Gompertz(mu=2, eta=3)")
+plot(x, bayesim::dgompertz(x, mu = 2, b = 0.1) - extraDistr::dgompertz(x, get_a(2, 0.1), 0.1), ylab = "Density difference", main = "Gompertz(mu=2, b=0.1)")
+plot(x, bayesim::dgompertz(x, mu = 1, b = 1) - extraDistr::dgompertz(x, get_a(1, 1), 1), ylab = "Density difference", main = "Gompertz(mu=1, b=1)")
+plot(x, bayesim::dgompertz(x, mu = 2, b = 3) - extraDistr::dgompertz(x, get_a(2, 3), 3), ylab = "Density difference", main = "Gompertz(mu=2, b=3)")
 
 # Quantile test
-x = seq(from = 0 , to = 1 , length.out=n)
+x = seq(from = 0.01, to = 0.99, length.out=n)
 layout(matrix(1:3, ncol = 3))
-plot(x, bayesim::qgompertz(x, mu = 2, eta = 0.1), type="l", ylab = "Quantile", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
-plot(x, bayesim::qgompertz(x, mu = 1, eta = 1), type="l", ylab = "Quantile", main = "apex-at-origin Gompertz(mu=1, eta=1)")
-plot(x, bayesim::qgompertz(x, mu = 2, eta = 3), type="l", ylab = "Quantile", main = "no-apex Gompertz(mu=2, eta=3)")
+plot(x, bayesim::qgompertz(x, mu = 2, b = 0.1), type="l", ylab = "Quantile", main = "Gompertz(mu=2, b=0.1)")
+plot(x, bayesim::qgompertz(x, mu = 1, b = 1), type="l", ylab = "Quantile", main = "Gompertz(mu=1, b=1)")
+plot(x, bayesim::qgompertz(x, mu = 2, b = 3), type="l", ylab = "Quantile", main = "Gompertz(mu=2, b=3)")
 
 # Quantile comparison to a reference
-x = seq(from = 0 , to = 1 , length.out=n)
+x = seq(from = 0.01, to = 0.99, length.out=n)
 layout(matrix(1:3, ncol = 3))
-plot(x, bayesim::qgompertz(x, mu = 2, eta = 0.1) - extraDistr::qgompertz(x, get_a(2, 0.1), get_b(2, 0.1)), ylab = "Quantile difference", main = "apex-after-origin Gompertz(mu=2, eta=0.1)")
-plot(x, bayesim::qgompertz(x, mu = 1, eta = 1) - extraDistr::qgompertz(x, get_a(1, 1), get_b(1, 1)), ylab = "Quantile difference", main = "apex-at-origin Gompertz(mu=1, eta=1)")
-plot(x, bayesim::qgompertz(x, mu = 2, eta = 3) - extraDistr::qgompertz(x, get_a(2, 3), get_b(2, 3)), ylab = "Quantile difference", main = "no-apex Gompertz(mu=2, eta=3)")
+plot(x, bayesim::qgompertz(x, mu = 2, b = 0.1) - extraDistr::qgompertz(x, get_a(2, 0.1), 0.1), ylab = "Quantile difference", main = "Gompertz(mu=2, b=0.1)")
+plot(x, bayesim::qgompertz(x, mu = 1, b = 1) - extraDistr::qgompertz(x, get_a(1, 1), 1), ylab = "Quantile difference", main = "Gompertz(mu=1, b=1)")
+plot(x, bayesim::qgompertz(x, mu = 2, b = 3) - extraDistr::qgompertz(x, get_a(2, 3), 3), ylab = "Quantile difference", main = "Gompertz(mu=2, b=3)")
 
 
 layout(matrix(1:3, ncol = 3))
-y = bayesim::rgompertz(n, mu = 2, eta = 0.1)
-hist(y, main = c(paste("Median:", median(y)), " for RNG of apex-after-origin Gompertz(mu=2, eta=0.1)"))
-y = bayesim::rgompertz(n, mu = 1, eta = 1)
-hist(y, main = c(paste("Median:", median(y)), " for RNG of apex-at-origin Gompertz(mu=1, eta=1)"))
-y = bayesim::rgompertz(n, mu = 2, eta = 3)
-hist(y, main = c(paste("Median:", median(y)), " for RNG of no-apex Gompertz(mu=2, eta=3)"))
+y = bayesim::rgompertz(n, mu = 2, b = 0.1)
+hist(y, main = c(paste("Median:", median(y)), " for RNG Gompertz(mu=2, b=0.1)"))
+y = bayesim::rgompertz(n, mu = 1, b = 1)
+hist(y, main = c(paste("Median:", median(y)), " for RNG Gompertz(mu=1, b=1)"))
+y = bayesim::rgompertz(n, mu = 2, b = 3)
+hist(y, main = c(paste("Median:", median(y)), " for RNG Gompertz(mu=2, b=3)"))
 
 # Now all the R code is tested, now test the BRMS family
 n = 1000
