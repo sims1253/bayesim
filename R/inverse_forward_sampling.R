b23f91189ab9297f6ee5352f20fe915d7f226cff
Maximilian Scholz <m0hawk@gmx.de>
2023-08-09 15:42:06 +0200

    feat: Add forward sampling for mixed lists of models

--- R/inverse_forward_sampling.R
+++ R/inverse_forward_sampling.R
@@ -1,23 +1,27 @@
 #' Simulate a new dataset using forward sampling.
 #'
-#' @param fit An object of class brmsfit.
+#' @param x An object of class brmsfit or a list of brmsfit objects.
 #' @param i The index of a single posterior draw to simulate a dataset for.
-#'          The index is passed to posterior_predict's "draw_ids" argument.
+#'          The index is passed to \code{\link{posterior_predict}}'s "draw_ids" argument.
 #' @param n The number of samples for the newly simulated dataset.
+#' @param ... Potential additional arguments.
 #'
 #' @return A data.frame containing n observations for each variable in the fit.
-#' @export
+#' @export forward_sampling forward_sampling.brmsfit forward_sampling.list
 #'
-#' @examples
-forward_sampling <- function(fit, i, n) {
-  if (i > ceiling(
-    (fit$fit@sim$iter - fit$fit@sim$warmup) / fit$fit@sim$thin * fit$fit@sim$chains
-  ) | i <= 0) {
+#'
+forward_sampling <- function(x, i, n, ...) {
+  UseMethod("forward_sampling")
+}
+
+#' @export
+forward_sampling.brmsfit <- function(x, i, n, ...) {
+  if (i > bayeshear::post_warmup_samples(x) | i <= 0) {
     stop("You tried to use a non-existent posterior sample.")
   }
 
-  model_formula <- fit$formula
-  if (is(fit$formula, "brmsformula")) {
+  model_formula <- x$formula
+  if (is(model_formula, "brmsformula")) {
     response <- model_formula$resp
     if (length(all.vars(model_formula$formula)) > 1) {
       stop("Your model does not contain responses for all used variables.")
@@ -28,7 +32,7 @@ forward_sampling <- function(fit, i, n) {
     ))
     colnames(df) <- c(response)
     df[response] <- as.vector(
-      brms::posterior_predict(fit,
+      brms::posterior_predict(x,
         newdata = df[response],
         resp = response,
         draw_ids = i
@@ -70,6 +74,61 @@ forward_sampling <- function(fit, i, n) {
           missing_responses <- missing_responses[!(missing_responses == response)]
         }
       }
+    }
+  }
+  return(df)
+}
+
+
+
+#' @export
+forward_sampling.list <- function(x, i, n, ...) {
+  if (any(lapply(x, bayeshear::post_warmup_samples) < i) | i <= 0) {
+    stop("You tried to use a non-existent posterior sample.")
+  }
+
+  resp_list <- list()
+  var_list <- list()
+
+  for (index in seq_along(x)) {
+    fit <- x[[index]]
+    if (is(fit$formula, "brmsformula")) {
+      resp_list[[fit$formula$resp]] <- index
+      var_list[[fit$formula$resp]] <- all.vars(fit$formula$formula)[-1]
+    } else if (is(fit$formula, "mvbrmsformula")) {
+      for (formula in fit$formula$forms) {
+        resp_list[[formula$resp]] <- index
+        var_list[[formula$resp]] <- all.vars(formula$formula)[-1]
+      }
+    } else {
+      stop("Unsupported model type detected!
+           Please use brmsfit or stanfit objects.")
+    }
+  }
+  df <- data.frame(matrix(0, # Create empty data frame
+    nrow = n,
+    ncol = length(resp_list)
+  ))
+  colnames(df) <- names(resp_list)
+
+  missing_responses <- names(resp_list)
+  while (length(missing_responses) > 0) {
+    success <- FALSE
+    for (response in missing_responses) {
+      if (length(intersect(missing_responses, var_list[[response]])) == 0) {
+        df[response] <- as.vector(
+          brms::posterior_predict(x[[resp_list[[response]]]],
+            newdata = df[var_list[[response]]],
+            resp = response,
+            draw_ids = i
+          )
+        )
+        missing_responses <- missing_responses[!(missing_responses == response)]
+        success <- TRUE
+      }
+    }
+    if (!success) {
+      stop("Unsolvable variable dependencies detected.")
     }
   }
   return(df)
